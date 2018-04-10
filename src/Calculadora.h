4656fe87bd50f448557c839365fe04bf852ea459
fdemarziani <florencia.demar@gmail.com>
2018-04-10 00:36:13 -0300

    operaciones aritmeticas

--- src/Calculadora.h
+++ src/Calculadora.h
@@ -2,6 +2,7 @@
 #define __CALCULADORA_H__
 
 #include "Programa.h"
+#include <stack>
 
 
 class Calculadora {
@@ -21,9 +22,55 @@ private:
     };
     vector<Variables> _memoria;
 
+    stack<int> pila;
+
     Programa _programa;
 
+
+    //las operaciones van acá??
+    void push(int n){
+        pila.push(n);
+    }
+
+    void add(){
+        if (pila.empty()){
+            pila.push(0);
+        } else if (pila.size() > 1){
+            int n = pila.top();
+            pila.pop();
+            int m = pila.top();
+            pila.pop();
+            pila.push(n+m);
+        }
+    }
+
+    void sub(){
+        if (pila.empty()){
+            pila.push(0);
+        } else if (pila.size() > 1){
+            int n = pila.top();
+            pila.pop();
+            int m = pila.top();
+            pila.pop();
+            pila.push(abs(n-m));
+        }
+    }
+
+    void mul(){
+        if (pila.empty()){
+            pila.push(0);
+        } else if (pila.size() > 1){  //no sé bien qué pasa si sólo hay un elemento
+            int n = pila.top();
+            pila.pop();
+            int m = pila.top();
+            pila.pop();
+            pila.push(n*m);
+        }
+    }
+
 };
+
+
 
 
 #endif //__CALCULADORA_H__
